795e60e38a01cbb30fbfdbab5fd168319f201fbe
4L1N <acristea63@gmail.com>
2020-01-16 17:42:22 +0100

    Updates

---
+++ precache-manifest.67aef557164f664b9c625e8b70bd0e83.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "019b57e91b11256f6eb4c39fcb10be53",
+    "url": "/englishcards/index.html"
+  },
+  {
+    "revision": "6824db69b3c1f952c616",
+    "url": "/englishcards/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "43f245366eedcd8f11fc",
+    "url": "/englishcards/static/js/2.193f51cd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/englishcards/static/js/2.193f51cd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6824db69b3c1f952c616",
+    "url": "/englishcards/static/js/main.72077131.chunk.js"
+  },
+  {
+    "revision": "911420e6d2dcefb78717",
+    "url": "/englishcards/static/js/runtime-main.668e2d72.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/englishcards/static/media/logo.5d5d9eef.svg"
+  }
+]);
